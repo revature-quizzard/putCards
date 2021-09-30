b9b81c56578ecd3a2cd1b3efd4008fa7de0ae8ea
kimatec <kimtranatec14@gmail.com>
2021-09-30 10:57:59 -0500

    first commit

---
+++ src/main/java/com/revature/put_cards/PutCardsHandler.java
@@ -0,0 +1,33 @@
+package com.revature.put_cards;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+public class PutCardsHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+
+    private static final Gson mapper = new GsonBuilder().setPrettyPrinting().create();
+    private final CardRepo cardRepo = new CardRepo();
+
+    @Override
+    public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent requestEvent, Context context){
+
+        LambdaLogger logger = context.getLogger();
+
+        logger.log("Received event: " + requestEvent);
+
+        String id = requestEvent.getQueryStringParameters().get("id");
+
+        cardRepo.deleteCardById(id);
+
+        APIGatewayProxyResponseEvent responseEvent = new APIGatewayProxyResponseEvent();
+        System.out.println("Deleting a card");
+
+        return responseEvent;
+    }
+}
